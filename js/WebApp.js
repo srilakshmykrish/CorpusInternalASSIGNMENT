f18c9c8e3b39ee08549aab7b3cf56bd0fe83687e
Srilakshmy <Srilakshmy@corpus.com>
2017-10-19 10:38:59 +0530

    Header title changed when particular sub-sub-category item is clicked. Title is just the class name.

--- js/WebApp.js
+++ js/WebApp.js
@@ -1,4 +1,11 @@
-document.getElementById("sss").onmouseover = function() 
+document.getElementById("header").onmouseover = function() 
 {
     this.style.backgroundColor = "light-green";
+}
+
+function GetSelectedItem(el) {
+	var output = document.getElementById('header');
+
+	alert(el.className);
+    output.innerHTML = el.className;
 }
